64edc38b284d05bd80cdf86fc2fc657862b26a17
Jesse Smith <jessesmith050@gmail.com>
2018-06-28 16:27:54 +0200

    getting there

--- final script.R
+++ final script.R
@@ -7,7 +7,7 @@
 library(tidyverse)
 library(ggpubr)
 library(lubridate)
-
+library(pgirmess)
 
 # load  data  -------------------------------------------------------------
 
@@ -24,8 +24,8 @@ pom_data <- read.csv("updated.csv", sep = ";")
 ggplot(pom_data, aes(x = pom)) +
   geom_density(aes(group = site, colour = site, fill = site),alpha = 0.3)
 
-# test for normality 
-shapiro.test(pom_data$pom)
+# test for normality
+# shapiro.test(pom_data$pom)
 # W = 0.64458, p-value < 2.2e-16
 # if p < 0.05 significantly different from normal
 # p > 0.05 not significantly different from normal (want?)
@@ -38,8 +38,8 @@ shapiro.test(pom_data$pom)
 # HO: POM content of the soil is lower on cleared beaches than on non-cleared beaches. 
 # H1: POM content of the soil is higher on cleared beaches than on non-cleared beaches.
 # or 
-# there is no significant difference in POM content between cleared and non-cleared sites. 
-# there is a significant difference in POM content betweeen cleared and uncleared areas? 
+# H0: there is no significant difference in POM content between cleared and non-cleared sites. 
+# H1: there is a significant difference in POM content betweeen cleared and uncleared areas? 
 
 # objectives:
 # 1. does POM differ between sites? 
@@ -58,10 +58,27 @@ sum_pom <-  pom_data %>%
 
 # site  -------------------------------------------------------------------
 
+# test for normality and homoscedasticity
+pom_data %>% 
+  group_by(site) %>% 
+  summarise(pom_data_dist = as.numeric(shapiro.test(pom)[2]), 
+            pom_data_var = var(pom))
+
+# # A tibble: 5 x 3
+#     site                 pom_data_dist pom_data_var
+# <fct>                        <dbl>        <dbl>
+#   1 Fish Hoek     0.000000000000000354       0.217 
+# 2 Hout Bay East 0.000000000000000120       0.137 
+# 3 Hout Bay West 0.000000000000799          0.147 
+# 4 Muizenberg    0.00000000000000501        0.452 
+# 5 Strandfontein 0.0000320                  0.0707
+# not normal 
+
+
 # pom content for each site, both cleared and non-cleared
 ggplot(data = pom_data, aes(x = site, y = pom, fill = area)) +
   geom_boxplot(outlier.shape = NA, notch = TRUE)+
-  coord_cartesian( ylim = c(0, 1.2)) +
+  coord_cartesian( ylim = c(0,1.2)) +
   labs(title = "POM content per site",
        x = "Site",
        y = "POM (g)") +
@@ -71,50 +88,44 @@ ggplot(data = pom_data, aes(x = site, y = pom, fill = area)) +
 kruskal.test(pom ~ as.factor(site), data = pom_data)
 
 # Kruskal-Wallis rank sum test
-# 
 # data:  pom by as.factor(site)
 # Kruskal-Wallis chi-squared = 34.619, df = 4, p-value = 5.561e-07
 
-# perform Tukey post-hoc test
-TukeyHSD(aov(pom ~ as.factor(site), data = pom_data))
+# perform Kruskalmc post-hoc test
+kruskalmc(pom ~ as.factor(site), data = pom_data)
 
-# Tukey multiple comparisons of means
-# 95% family-wise confidence level
-# 
-# Fit: aov(formula = pom ~ as.factor(site), data = pom_data)
-# 
-# $`as.factor(site)`
-# diff        lwr       upr     p adj
-# Hout Bay East-Fish Hoek     -0.0271128571 -0.1766278 0.1224021 0.9876703
-# Hout Bay West-Fish Hoek      0.0445400000 -0.1197154 0.2087954 0.9463568
-# Muizenberg-Fish Hoek        -0.0005706173 -0.1760043 0.1748631 1.0000000
-# Strandfontein-Fish Hoek     -0.1891200000 -0.6046569 0.2264169 0.7241590
-# Hout Bay West-Hout Bay East  0.0716528571 -0.0949325 0.2382382 0.7641247
-# Muizenberg-Hout Bay East     0.0265422399 -0.1510749 0.2041594 0.9941018
-# Strandfontein-Hout Bay East -0.1620071429 -0.5784705 0.2544562 0.8242792
-# Muizenberg-Hout Bay West    -0.0451106173 -0.2353025 0.1450813 0.9667011
-# Strandfontein-Hout Bay West -0.2336600000 -0.6556397 0.1883197 0.5523867
-# Strandfontein-Muizenberg    -0.1885493827 -0.6150046 0.2379058 0.7452096
+# Multiple comparison test after Kruskal-Wallis 
+# p.value: 0.05 
+# Comparisons
+# obs.dif critical.dif difference
+# Fish Hoek-Hout Bay East      31.39452     45.85033      FALSE
+# Fish Hoek-Hout Bay West      70.92667     50.37064       TRUE
+# Fish Hoek-Muizenberg         31.22395     53.79859      FALSE
+# Fish Hoek-Strandfontein      91.32333    127.42875      FALSE
+# Hout Bay East-Hout Bay West  39.53214     51.08515      FALSE
+# Hout Bay East-Muizenberg     62.61847     54.46816       TRUE
+# Hout Bay East-Strandfontein 122.71786    127.71288      FALSE
+# Hout Bay West-Muizenberg    102.15062     58.32434       TRUE
+# Hout Bay West-Strandfontein 162.25000    129.40451       TRUE
+# Muizenberg-Strandfontein     60.09938    130.77697      FALSE
+
+# true is where the differences lie 
 
 
-# incomplete ...
-# use anova for sites
-# anova1 <- aov(pom~site,data= pom_data)
-# 
-# summary(anova1)
-# Df Sum Sq Mean Sq F value Pr(>F)
-# site          4   0.66  0.1639   0.759  0.552
-# Residuals   476 102.77  0.2159   
-
-# Terms:
-#   site Residuals
-# Sum of Squares    0.65558 102.76541
-# Deg. of Freedom         4       476
-# 
-# Residual standard error: 0.4646437
-# Estimated effects may be unbalanced
 
 # area --------------------------------------------------------------------
+
+# test for normality and homoscedasticity
+pom_data %>% 
+  group_by(area) %>% 
+  summarise(pom_data_dist = as.numeric(shapiro.test(pom)[2]), 
+            pom_data_var = var(pom))
+ 
+# # A tibble: 2 x 3
+# area                      pom_data_dist pom_data_var
+# <fct>                             <dbl>        <dbl>
+# 1 cleared     0.00000000000000000000538          0.176
+# 2 non_cleared 0.0000000000000000000000531        0.256
 
 #trial and error 
 
@@ -143,42 +154,33 @@ ggplot(data = pom_data, aes(x = area, y = pom, fill = area)) +
        y = "POM (g)") +
   theme_minimal()  
 
-#t-test
-t.test(pom ~ area, data = pom_data)
+# wilcox test
+compare_means(pom ~ area, data = pom_data, method = "wilcox")
 
-# Welch Two Sample t-test
-# 
-# data:  pom by area
-# t = 0.23637, df = 459.6, p-value = 0.8132
-# alternative hypothesis: true difference in means is not equal to 0
-# 95 percent confidence interval:
-#   -0.07339485  0.09346519
-# sample estimates:
-#   mean in group cleared mean in group non_cleared 
-# 0.3489259                 0.3388908 
+# # A tibble: 1 x 8
+# .y.   group1  group2           p  p.adj p.format p.signif method  
+# <chr> <chr>   <chr>        <dbl>  <dbl> <chr>    <chr>    <chr>   
+#   1 pom   cleared non_cleared 0.0389 0.0389 0.039    *        Wilcoxon
+
+#means between area is significantly different 
+
 #  we do not reject (accept) the null hypothesis that there is a difference between cleared and non-cleared area.
 
 
-# Kruskal-Wallis
-kruskal.test(pom ~ as.factor(area), data = pom_data)
-
-# Kruskal-Wallis rank sum test
-# 
-# data:  pom by as.factor(area)
-# Kruskal-Wallis chi-squared = 4.2674, df = 1, p-value = 0.03885
-
-# perform Tukey post-hoc test
-TukeyHSD(aov(pom ~ as.factor(month), data = pom_data))
-
-# Fit: aov(formula = pom ~ as.factor(month), data = pom_data)
-# 
-# $`as.factor(month)`
-#               diff        lwr        upr     p adj
-# Feb-Apr -0.01954468 -0.2335829 0.19449350 0.9749139
-# Mar-Apr -0.08948483 -0.1921009 0.01313125 0.1015684
-# Mar-Feb -0.06994014 -0.2854967 0.14561646 0.7259956
-
 # time  -------------------------------------------------------------------
+
+# test for normality and homoscedasticity
+pom_data %>% 
+  group_by(month) %>% 
+  summarise(pom_data_dist = as.numeric(shapiro.test(pom)[2]), 
+            pom_data_var = var(pom))
+
+# # A tibble: 3 x 3
+# month               pom_data_dist pom_data_var
+# <fct>                       <dbl>        <dbl>
+#   1 Apr    0.00000000000000000000150         0.219
+# 2 "Feb " 0.0000797                         0.164
+# 3 "Mar " 0.000000000000000000000215        0.216
 
 # removing the site Strandfontein due to it only being sampled once 
 time <- pom_data[-c(21:30), ]
@@ -197,44 +199,110 @@ ggplot(data = time, aes(x = month, y = pom, fill = area)) +
 kruskal.test(pom ~ as.factor(month), data = time)
 
 # Kruskal-Wallis rank sum test
-# 
 # data:  pom by as.factor(month)
 # Kruskal-Wallis chi-squared = 17.931, df = 2, p-value = 0.0001278
 
-# perform Tukey post-hoc test
-TukeyHSD(aov(pom ~ as.factor(month), data = time))
+# perform Kruskalmc post-hoc test
+kruskalmc(pom ~ as.factor(month), data = time)
 
-#   Tukey multiple comparisons of means
-# 95% family-wise confidence level
-# 
-# Fit: aov(formula = pom ~ as.factor(month), data = time)
-# 
-# $`as.factor(month)`
-#              diff        lwr        upr     p adj
-# Feb-Apr -0.01954468 -0.2349895 0.19590016 0.9752330
-# Mar-Apr -0.08267323 -0.1873124 0.02196598 0.1523290
-# Mar-Feb -0.06312854 -0.2807471 0.15449000 0.7740766
+# Multiple comparison test after Kruskal-Wallis 
+# p.value: 0.05 
+# Comparisons
+# obs.dif critical.dif difference
+# Apr-Feb   22.29425     64.05939      FALSE
+# Apr-Mar   55.01370     31.11295       TRUE
+# Feb -Mar  32.71944     64.70571      FALSE
 
+# sig diff between apr - mar
 
 # transect lengths  -------------------------------------------------------
 
 meta <- read.csv2("metadata.csv") 
 
+meta_long <- meta %>% 
+  gather(key = "variable", value = "value", -site, -date, -month, -beach_cast_cleared, - beach_cast_noncleared)
+
+# test fot normality
+
+# meta_long %>% 
+#   group_by(site) %>% 
+#   summarise(meta_long = as.numeric(shapiro.test(value)[2]))
+#??? Error in summarise_impl(.data, dots) : 
+#   Evaluation error: sample size must be between 3 and 5000.
+
+shapiro.test(meta_long$value)
+# Shapiro-Wilk normality test
+# data:  meta_long$value
+# W = 0.97173, p-value = 0.03587
+
+#test for homoscedasticity 
+meta_long %>% 
+  #group_by(site) %>% 
+  summarise(meta_long_var = var(value))
+
+# meta_long_var
+# 1      18.90252
+
+# # A tibble: 5 x 2
+# site                meta_long_var
+# <fct>                       <dbl>
+#   1 Fish Hoek                    15.0
+# 2 Hout Bay East                11.8
+# 3 "Hout Bay West "             14.1
+# 4 Muizenberg                   29.6
+# 5 Strandfontein beach          18.0           
 
 
-## Cleeared transects
+# total transect length
+ggplot(data = meta_long, aes(x = variable, y = value, fill = variable)) +
+  geom_boxplot(outlier.shape = NA, notch = TRUE)+
+  labs(title = "Total transect lengths",
+       x = "area",
+       y = "transect length (m)") +
+  theme_minimal()  
+
+# total transect length per site
+ggplot(data = meta_long, aes(x = site, y = value, fill = variable)) +
+  geom_boxplot(outlier.shape = NA, notch = TRUE)+
+  labs(title = "transect lengths per site",
+       x = "area",
+       y = "transect length (m)") +
+  theme_minimal() 
+
+# wilcox test
+compare_means(value ~ variable, data = meta_long, method = "wilcox")
+
+# A tibble: 1 x 8
+# .y.   group1           group2                   p p.adj p.format p.signif method  
+# <chr> <chr>            <chr>                <dbl> <dbl> <chr>    <chr>    <chr>   
+#   1 value cleared_transect non_cleared_transect 0.106 0.106 0.11     ns       Wilcoxon
+
+# means not sig diff
+
+## Cleared transects
 
 # quick visualisation 
 ggplot(meta, aes(x = cleared_transect)) +
   geom_density(aes(group = site, colour = site, fill = site),alpha = 0.3)
 
-# test for normality 
-shapiro.test(meta$cleared_transect)
+# test for normality and homoscedasticity - site
+meta %>% 
+  group_by(site) %>% 
+  summarise(len_data_dist = as.numeric(shapiro.test(cleared_transect)[2]), 
+            len_data_var = var(cleared_transect))
 
-# Shapiro-Wilk normality test
-# data:  pom_meta$cleared_transect
-# W = 0.91412, p-value = 0.001856
-# not normal 
+# test for normality and homoscedasticity - month
+meta %>% 
+  group_by(month) %>% 
+  summarise(meta_dist = as.numeric(shapiro.test(cleared_transect)[2]), 
+            meta_var = var(cleared_transect))
+
+#  A tibble: 3 x 3
+# month meta_dist meta_var
+# <fct>     <dbl>    <dbl>
+# 1 Apr     0.0713     16.4 
+# 2 Feb     0.463       4.33
+# 3 Mar     0.00812    18.8 
 
 sum_cleared_trans <-  meta %>% 
   group_by(site) %>% 
@@ -262,19 +330,53 @@ kruskal.test(cleared_transect ~ as.factor(site), data = meta)
 # data:  cleared_transect by as.factor(site)
 # Kruskal-Wallis chi-squared = 6.5546, df = 4, p-value = 0.1614
 
+# perform Kruskalmc post-hoc test
+kruskalmc(pom ~ as.factor(site), data = meta)
+
+# Tukey multiple comparisons of means
+# 95% family-wise confidence level
+# Fit: aov(formula = cleared_transect ~ as.factor(site), data = meta)
+# $`as.factor(site)`
+# diff         lwr      upr     p adj
+# Hout Bay East-Fish Hoek             1.90952381  -2.1563516 5.975399 0.6700437
+# Hout Bay West -Fish Hoek            1.96666667  -2.5000570 6.433390 0.7203974
+ # Muizenberg-Fish Hoek                5.14166667   0.3516392 9.931694 0.0299052
+# Strandfontein beach-Fish Hoek      -2.73333333 -14.0333497 8.566683 0.9578892
+# Hout Bay West -Hout Bay East        0.05714286  -4.4729417 4.587227 0.9999996
+# Muizenberg-Hout Bay East            3.23214286  -1.6170230 8.081309 0.3340943
+# Strandfontein beach-Hout Bay East  -4.64285714 -15.9680687 6.682354 0.7697352
+# Muizenberg-Hout Bay West            3.17500000  -2.0148639 8.364864 0.4201891
+# Strandfontein beach-Hout Bay West  -4.70000000 -16.1752208 6.775221 0.7703307
+# Strandfontein beach-Muizenberg     -7.87500000 -19.4798885 3.729888 0.3165357
+
+
 ## non_cleared transects
 
 # quick visualisation 
 ggplot(meta, aes(x = non_cleared_transect)) +
   geom_density(aes(group = site, colour = site, fill = site),alpha = 0.3)
 
-# test for normality 
-shapiro.test(meta$non_cleared_transect)
 
-# Shapiro-Wilk normality test
-# data:  pom_meta$non_cleared_transect
-# W = 0.98281, p-value = 0.6987
-# normal 
+# test for normality and homoscedasticity - month 
+meta %>% 
+  group_by(month) %>% 
+  summarise(meta_dist = as.numeric(shapiro.test(non_cleared_transect)[2]), 
+            meta_var = var(non_cleared_transect))
+
+# A tibble: 3 x 3
+# month meta_dist meta_var
+# <fct>     <dbl>    <dbl>
+# 1 Apr       0.340    23.9 
+# 2 Feb       0.637    2.33
+# 3 Mar       0.312    14.9
+
+# test for normality and homoscedasticity - site
+meta %>% 
+  group_by(site) %>% 
+  summarise(meta_dist = as.numeric(shapiro.test(non_cleared_transect)[2]), 
+            meta_var = var(non_cleared_transect))
+# ERROR
+
 
 sum_non_cleared_trans <-  meta %>% 
   group_by(site) %>% 
@@ -284,6 +386,12 @@ sum_non_cleared_trans <-  meta %>%
             max_non = max(non_cleared_transect), 
             sd_non = sd(non_cleared_transect))
 
+shapiro.test(meta$non_cleared_transect)
+
+# Shapiro-Wilk normality test
+# data:  meta$non_cleared_transect
+# W = 0.98281, p-value = 0.6987
+# normal 
 
 # transect lengths for each site, non_cleared
 ggplot(data = meta, aes(x = site, y = non_cleared_transect)) +
@@ -295,8 +403,22 @@ ggplot(data = meta, aes(x = site, y = non_cleared_transect)) +
        y = "transect lengths (m)") +
   theme_minimal()  
 
-summary(aov(mn_non ~ as.factor(site), data = sum_non_cleared_trans))
 
-# notes  ------------------------------------------------------------------
+summary(aov(non_cleared_transect ~ as.factor(site), data = meta))
+# Df Sum Sq Mean Sq F value Pr(>F)  
+# as.factor(site)  4  184.3   46.09   2.537 0.0537 .
+# Residuals       43  781.1   18.17                 
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-# tukey test specifies where the differences lie, whereas kruskal just states that there is a difference
+
+# correlations --------------------------------------------------------------
+
+# between pom and transect lengths
+#   pom and kelp estimates 
+
+library(corrplot)
+
+# merge data 
+
+# cor.test(ecklonia$primary_blade_length, ecklonia$primary_blade_width, method = "kendall")
